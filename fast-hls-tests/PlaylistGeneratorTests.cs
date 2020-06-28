f31225edda999746e86658e3ae479624840375c4
Jeroen Vannevel <jeroen.vannevel@hudl.com>
2020-06-28 11:36:55 +0100

    cleaned up tests

--- fast-hls-tests/PlaylistGeneratorTests.cs
+++ fast-hls-tests/PlaylistGeneratorTests.cs
@@ -6,6 +6,19 @@ using Xunit;
 
 namespace FastHlsTests
 {
+    internal static class PlaylistGeneratorExtensions
+    {
+        public static async Task AssertGeneratedContent(this PlaylistGenerator generator, string content)
+        {
+            var outputStream = new MemoryStream();
+            await generator.WriteToStream(outputStream);
+            outputStream.Position = 0;
+            var output = await new StreamReader(outputStream).ReadToEndAsync();
+
+            Assert.Equal(content, output);
+        }
+    }
+
     public class PlaylistGeneratorTests
     {
         private PlaylistGenerator generator = new PlaylistGenerator();
@@ -14,31 +27,22 @@ namespace FastHlsTests
         public async Task WritesHeader()
         {
             generator.Start(PlaylistType.VOD, version: 8, targetDuration: 10);
-            var outputStream = new MemoryStream();
-            await generator.WriteToStream(outputStream);
-            outputStream.Position = 0;
-            var output = await new StreamReader(outputStream).ReadToEndAsync();
-
-            Assert.Equal(@"#EXTM3U
+            await generator.AssertGeneratedContent(@"#EXTM3U
 #EXT-X-PLAYLIST-TYPE:VOD
 #EXT-X-TARGETDURATION:10
 #EXT-X-VERSION:8
 #EXT-X-MEDIA-SEQUENCE:0
-", output);
+");
         }
 
         [Fact]
-        public async Task WritesMediaFiles() {
+        public async Task WritesMediaFiles()
+        {
             generator.Start(PlaylistType.VOD, version: 8, targetDuration: 10);
             generator.AddMediaFile("http://example.com/movie1/fileSequenceA.ts", duration: 10);
             generator.AddMediaFile("http://example.com/movie1/fileSequenceB.ts", duration: 10);
 
-            var outputStream = new MemoryStream();
-            await generator.WriteToStream(outputStream);
-            outputStream.Position = 0;
-            var output = await new StreamReader(outputStream).ReadToEndAsync();
-
-            Assert.Equal(@"#EXTM3U
+            await generator.AssertGeneratedContent(@"#EXTM3U
 #EXT-X-PLAYLIST-TYPE:VOD
 #EXT-X-TARGETDURATION:10
 #EXT-X-VERSION:8
@@ -47,22 +51,18 @@ namespace FastHlsTests
 http://example.com/movie1/fileSequenceA.ts
 #EXTINF:10.0,
 http://example.com/movie1/fileSequenceB.ts
-", output);
+");
         }
 
         [Fact]
-        public async Task WritesEndTag() {
+        public async Task WritesEndTag()
+        {
             generator.Start(PlaylistType.VOD, version: 8, targetDuration: 10);
             generator.AddMediaFile("http://example.com/movie1/fileSequenceA.ts", duration: 10);
             generator.AddMediaFile("http://example.com/movie1/fileSequenceB.ts", duration: 10);
             generator.Finish();
 
-            var outputStream = new MemoryStream();
-            await generator.WriteToStream(outputStream);
-            outputStream.Position = 0;
-            var output = await new StreamReader(outputStream).ReadToEndAsync();
-
-            Assert.Equal(@"#EXTM3U
+            await generator.AssertGeneratedContent(@"#EXTM3U
 #EXT-X-PLAYLIST-TYPE:VOD
 #EXT-X-TARGETDURATION:10
 #EXT-X-VERSION:8
@@ -71,7 +71,7 @@ http://example.com/movie1/fileSequenceB.ts
 http://example.com/movie1/fileSequenceA.ts
 #EXTINF:10.0,
 http://example.com/movie1/fileSequenceB.ts
-#EXT-X-ENDLIST", output);
+#EXT-X-ENDLIST");
         }
     }
 }
