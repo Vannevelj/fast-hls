33514b7e94a353b565806a81e262ca444f162d11
Vannevelj <jer_vannevel@outlook.com>
2020-07-26 23:58:53 +0100

    preloadhint render

--- fast-hls/Models/PreloadHint.cs
+++ fast-hls/Models/PreloadHint.cs
@@ -1,10 +1,32 @@
+using System.Text;
+using FastHls.Models.Interfaces;
+
 namespace FastHls.Models
 {
-    public struct PreloadHint
+    public struct PreloadHint : IManifestItem
     {
         public HintType Type { get; set; }
         public string Path { get; set; }
         public int? Start { get; set; }
         public int? Length { get; set; }
+
+        public string Render()
+        {
+            var builder = new StringBuilder();
+            builder.Append($"#EXT-X-PRELOAD-HINT:TYPE={Type},URI=\"{Path}\"");
+
+            if (Start.HasValue)
+            {
+                builder.Append($",BYTERANGE-START=\"{Start.Value}\"");
+            }
+
+            if (Length.HasValue)
+            {
+                builder.Append($",BYTERANGE-LENGTH=\"{Length.Value}\"");
+            }
+
+            builder.AppendNormalizedNewline();
+            return builder.ToString();
+        }
     }
 }
